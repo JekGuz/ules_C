2dec80fd8d18476e1037fcf224b2bbb9d6e0e741
«JekGuz» <jekaterina.guzek@gmail.com>
2025-04-16 14:25:22 +0300

    kordused

--- Main_Class.cs
+++ Main_Class.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,45 @@ namespace ules_C
 {
     internal class Main_Class
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
+            // 2 OSA kordused, Massivid, Listid, Klassid
+            // FOR i
+            Console.WriteLine("------FOR-UP------");
+            int i = 0;
+            for (i = 0; i < 10; i++) // увеличиваем
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("-------FOR-DOWN------");
+
+            for (int j = i; j > 0; j--)  // уменьшаем
+            {
+                Console.WriteLine(j);
+            }
+            // WHILE (true)
+            Console.WriteLine("------WHILE-(i>0)---------");
+            while (i > 0)
+            {
+                Console.WriteLine(i);
+                i --;
+            }
+            Console.WriteLine("------DO---------");
+            Console.WriteLine("backspace");
+            ConsoleKeyInfo key = new ConsoleKeyInfo();
+            do 
+            {
+                Console.WriteLine("Vajuta Backspace");
+                key = Console.ReadKey();
+            }
+            while (key.Key != ConsoleKey.Backspace);
+
+            // 3 OSA Faillid, Andmetüüdb, IF, CASE, RANDOM, Alamfunktioonid
+
+
+
+            /* 1 OSA
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.OutputEncoding = Encoding.UTF8;
@@ -17,87 +55,51 @@ namespace ules_C
             // I. osa Andmetyybid, IF, Case, Random, Almafunktsioonid
             /*
              Lisame siia kommentaari
-             */
+            
             int a = 0;
-            string tekst = "Python to C#";
+            string tekst = string.Empty;
+            string tekst2 = string.Empty;
             char taht = 'P';
             double arv = 5.23456787541;
             float arv1 = 2;
+            float arv5 = 1;
+            string vastus = string.Empty;
+            float vastus1 = 1;
+
+            // remont
+            Console.WriteLine("Otsustame renoveerimise üle!");
+            vastus1 = funktionide_Class_1_osa.ristkulik(arv1, arv5);
+            Console.WriteLine(vastus1);
+
+            // Küsi kahe inimese nimed ning teata, et nad on täna pinginaabrid
+            Console.WriteLine("Tere tulemas!");
+            vastus = funktionide_Class_1_osa.nabrid(tekst, tekst2);
+            Console.WriteLine(vastus);
+
+            // juku
             Console.Write("Mis on sinu nimi? ");
             tekst = Console.ReadLine();
             Console.WriteLine("Tere! \n" + tekst);
-            Console.WriteLine("Tere! \n {0}", tekst);
-            if (tekst.ToLower() == "juku")
-            {
-                Console.WriteLine("Lähme kinno!");
-                try
-                {
-                    Console.WriteLine("{0} \nKui vana sa oled?", tekst);
-                    int vanus = int.Parse(Console.ReadLine());
-                    if (vanus <= 0 || vanus > 100) // || - or, && - and
-                    {
-                        Console.WriteLine("Viga!");
-                    }
-                    else if (vanus > 0 && vanus <= 6) 
-                    {
-                        Console.WriteLine("Tasura");
-                        Console.BackgroundColor= ConsoleColor.Green;
-                    }
-                    else if (vanus <= 15)
-                    {
-                        Console.WriteLine("Lastepilet");
-                        Console.BackgroundColor = ConsoleColor.Magenta;
-                    }
-                    else if (vanus <= 65)
-                    {
-                        Console.WriteLine("Täispilet");
-                        Console.BackgroundColor = ConsoleColor.DarkRed;
-                    }
-                    else if (vanus <= 100)
-                    {
-                        Console.WriteLine("Sooduspilet");
-                        Console.BackgroundColor = ConsoleColor.White;
-                    }
+            vastus = funktionide_Class_1_osa.juku(tekst); 
+            Console.WriteLine(vastus);
 
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                }
-            }
-            else
-            {
-                Console.WriteLine("Olen hõivatud!");
-            }
-
-
+            // kalkulatir
             Console.Write("Arv 2: ");
             int arv2 = int.Parse(Console.ReadLine());
             // Console.WriteLine("Arvude {0} ja {1} korrutis võrdub {2}", arv1, arv2, arv1 + arv2);
-            arv1 = funktionide_Class.Kalkulaator(a, arv2);
+            arv1 = funktionide_Class_1_osa.Kalkulaator(a, arv2);
             Console.WriteLine(arv1);
 
+            // nadalapaevad
             Console.WriteLine("Switch'i kasutamine");
             Random rnd = new Random();
             a = rnd.Next(1, 7);
             Console.WriteLine(a);
-            switch (a)
-            {
-                case 1: tekst = "Esmaspäev";break;
-                case 2: tekst = "Teisipäev";break;
-                case 3: tekst = "Kolmapäev";break;
-                case 4: tekst = "Neljapäev";break;
-                case 5: tekst = "Reedel";break;
-                case 6: tekst = "Laupäev";break;
-                case 7: tekst = "Puhapäev";break;
-
-                default:
-                    tekst = "Tundmatu";
-                    break;
-            }
+            tekst = funktionide_Class_1_osa.nadalapaeva(a);
             Console.WriteLine(tekst);
 
             Console.ReadKey();
+             */
         }
     }
 }
---
+++ funktionide_Class_2_osa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ules_C
+{
+    internal class funktionide_Class_2_osa
+    {
+    }
+}
